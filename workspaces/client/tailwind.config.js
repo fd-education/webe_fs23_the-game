cc02d7fa640f01a2e510c8a058a1add7b2bdcdef
Fabian Diemand <fabian.diemand@students.ffhs.ch>
2023-04-20 00:50:57 +0200

    feature(auth): rename colors to better differentiate between library and own colors

--- workspaces/client/tailwind.config.js
+++ workspaces/client/tailwind.config.js
@@ -6,34 +6,32 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                primaryLight: "#F3E6E6",
-                secondaryLight: "#FFFFFF",
-                shadowLight: "#EFE0EF",
+                primaryLight: '#F3E6E6',
+                secondaryLight: '#FFFFFF',
+                shadowLight: '#EFE0EF',
 
-                primaryDark: "#3D3A3A",
-                secondaryDark: "#3A3737",
-                shadowDark: "rgba(33,33,33,0.25)",
+                primaryDark: '#3D3A3A',
+                secondaryDark: '#3A3737',
+                shadowDark: 'rgba(33,33,33,0.25)',
 
-                purple: "#988ED7",
-                darkPurple: "#6f65a0",
+                the_game_purple: '#6B4EFF',
+                the_game_darkPurple: '#6f65a0',
 
-                orange: "#BF7160",
-                darkOrange: "#814d41",
-                gray: "#847C7C",
+                the_game_orange: '#BF7160',
+                the_game_darkOrange: '#814d41',
+                the_game_gray: '#847C7C',
 
-                chatBubbleForeign: "#262628",
-                chatBubbleOwn: "#E9E9EB"
+                chatBubbleForeign: '#262628',
+                chatBubbleOwn: '#E9E9EB'
             },
             boxShadow: {
-                "around": "0 0 7px 11px theme(colors.shadowDark)"
+                around: '0 0 7px 11px theme(colors.shadowDark)'
             },
             borderRadius: {
-                "2lg": "1rem"
+                '2lg': '1rem'
             }
-        },
+        }
     },
     variants: {},
-    plugins: [
-        require('daisyui')
-    ],
-}
+    plugins: [require('daisyui')]
+};
